4ae71e49c45425137f06890dffbad8938dfbef6d
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-04 22:05:53 -0400

    Implementar la clase Usuario y se finalizo la etapa de modelo.

---
+++ src/main/java/com/api/apigateway/jwt/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.api.apigateway.jwt.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private int idUsuario;
+
+    @Column(name = "nome")
+    private String nombreUsuario;
+
+    private String contrasena;
+    private String estado;
+    private String rol;
+}
