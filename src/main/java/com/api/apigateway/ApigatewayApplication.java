389c61df8e78febad0cbee4d3995ae8d48b67a88
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-09 12:45:07 -0400

    Se combino la branch con la branch main.

--- src/main/java/com/api/apigateway/ApigatewayApplication.java
+++ src/main/java/com/api/apigateway/ApigatewayApplication.java
@@ -3,7 +3,7 @@ package com.api.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.api.apigateway")
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
